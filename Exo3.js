5a2942a636de1b22a565aab4703a9e0e21cda5ca
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2021-02-22 16:10:54 +0100

    correction valMax

--- Exo3.js
+++ Exo3.js
@@ -7,8 +7,13 @@ for (i = 0; i < 5; i++) {
     console.log('Please input a number');
     var number = scanf('%d');
     tableau.push(number);
+
+    if (tableau[i] > valeurMax) {
+        valeurMax = tableau[i];
+    }
 }
 
 console.log('tableau ' + tableau);
 console.log('La valeur max du tableau est ' + valeurMax);
 console.log("Elle se situe à l'indice " + tableau.indexOf(valeurMax));
+
