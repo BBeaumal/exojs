29c758b5687ad3310bcd28e1f4f83f4c6e3296bf
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2021-02-22 14:27:03 +0100

    Correction text + format

--- Exo1.js
+++ Exo1.js
@@ -1,14 +1,14 @@
-const scanf =require('scanf');
+const scanf = require('scanf');
 
 console.log('Please input the first word');
 var firstWord = scanf('%s');
 // console.log(firstWord);
 
-console.log('Please the second word')
-var secondWord=scanf('%s');
+console.log('Please input the second word')
+var secondWord = scanf('%s');
 // console.log(secondWord);
 
-if (firstWord.length < 2*secondWord.length){
+if (firstWord.length < 2 * secondWord.length) {
     console.log('Mot trop court');
 } else {
     console.log('Mot trop long');
