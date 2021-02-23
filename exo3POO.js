ff5ebb6386cf6c47f6356f52f502dcbd1bfd9229
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2021-02-23 12:40:51 +0100

    correction constructor

--- exo3POO.js
+++ exo3POO.js
@@ -1,8 +1,9 @@
-const Point = require('./exo2POO');
+const Point = require('./exo2POO.js');
 
 class Circle {
-    constructor(Point, radius) {
-        this.Point = Point;
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y
         this.radius = radius;
     }
 
@@ -10,8 +11,8 @@ class Circle {
         return Math.pow(`${this.radius}`, 2) * Math.PI;
     }
     containsPoint(point) {
-        let dist = Math.sqrt(Math.pow((point.membreX - this.Point.membreX), 2) +
-            Math.pow(point.membreY - this.Point.membreY, 2));
+        let dist = Math.sqrt(Math.pow((point.membreX - this.x), 2) +
+            Math.pow(point.membreY - this.y, 2));
         if (dist = 0) {
             return true;
         }
@@ -19,7 +20,7 @@ class Circle {
     }
 }
 
-Point pointA = new Point(2, 3);
-Circle cercle1 = new Circle(pointA, 5);
+Point.pointA = new Point(2, 3);
+Circle cercle1 = new Circle(1, 2, 5);
 
 
