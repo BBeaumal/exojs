1119002dd0d88e68d658d2e8334d50f7a849c566
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2021-02-23 12:36:51 +0100

    ADD contains

--- exo3POO.js
+++ exo3POO.js
@@ -7,10 +7,15 @@ class Circle {
     }
 
     area() {
-        return (`${this.radius}` ^ 2) * 3.14;
+        return Math.pow(`${this.radius}`, 2) * Math.PI;
     }
-    containsPoint(point){
-        
+    containsPoint(point) {
+        let dist = Math.sqrt(Math.pow((point.membreX - this.Point.membreX), 2) +
+            Math.pow(point.membreY - this.Point.membreY, 2));
+        if (dist = 0) {
+            return true;
+        }
+        return false;
     }
 }
 
