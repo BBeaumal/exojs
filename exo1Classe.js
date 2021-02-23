c859df928148b41e5eb816eae37a316d22241fa4
Baptiste Beaumal <baptiste.beaumal@gmail.com>
2021-02-23 11:15:51 +0100

    ADD method swim and verification tusksize

--- exo1Classe.js
+++ exo1Classe.js
@@ -5,20 +5,23 @@ class Hippotamus {
         this.tuskSize = tuskSize;
     }
     toString() {
-      console.log('Hippotamus ', this.name, ' weight ', this.weight, 'kg and has tusks of ', this.tuskSize, 'cm');
+        console.log('Hippotamus ', this.name, ' weight ', this.weight, 'kg and has tusks of ', this.tuskSize, 'cm');
     }
     swim() {
-       return this.weight = this.weight - 0,3;
-         console.log(this.weight);
+        this.weight = this.weight - (0.3);
+        console.log(this.weight);
     }
     eat() {
-      this.weight++;
-    //    console.log(this.weight);
+        this.weight++;
+        //    console.log(this.weight);
     }
 
     fight(Hippotamus) {
-        if (this.tuskSize > Hippotamus.tuskSize) {
-           console.log(this.name, 'won');
+        if (this.tuskSize && Hippotamus.tuskSize == 0) {
+            console.log('Tusksize cant be negative')
+        }
+        else if (this.tuskSize > Hippotamus.tuskSize) {
+            console.log(this.name, 'won');
         } else {
             console.log(Hippotamus.name, 'won');
         }
@@ -27,8 +30,8 @@ class Hippotamus {
 
 var hippo1 = new Hippotamus("Toto", 600, 20);
 hippo1.toString();
-hippo1.eat();
+hippo1.swim();
 hippo1.toString();
 var hippo2 = new Hippotamus("Dodo", 400, 50);
 
-// hippo1.fight(hippo2);
+hippo1.fight(hippo2);
